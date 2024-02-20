53669c5cb9d982dd8026a1b89ca67eb8853305ad
Chayut <147202895+Did85258@users.noreply.github.com>
2024-02-20 11:57:11 +0700

    Add files via upload

---
+++ test/src/main/java/soa/service/TestService.java
@@ -0,0 +1,17 @@
+package soa.service;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestService {
+	
+	
+	@GetMapping("/test")
+	public String test() {
+		
+		
+		return "spring - test" ;
+	}
+
+}
